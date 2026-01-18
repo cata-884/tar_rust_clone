2da4a83e35ee98ac1dac0f4fa5c6610b77c20309
cata-884 <catalin.rusu884@gmail.com>
2026-01-18 21:04:38 +0200

    added gitignore and removed artifacts

--- target/debug/deps/tar_project-9c7dde16a889f215.d
+++
@@ -1,8 +0,0 @@
-/home/cata-884/rust_tar/target/debug/deps/tar_project-9c7dde16a889f215.d: src/main.rs src/header.rs src/archive.rs src/extract.rs
-
-/home/cata-884/rust_tar/target/debug/deps/libtar_project-9c7dde16a889f215.rmeta: src/main.rs src/header.rs src/archive.rs src/extract.rs
-
-src/main.rs:
-src/header.rs:
-src/archive.rs:
-src/extract.rs:
--- target/debug/deps/tar_project-cdd244b994f2a8e0.d
+++
@@ -1,8 +0,0 @@
-/home/cata-884/rust_tar/target/debug/deps/tar_project-cdd244b994f2a8e0.d: src/main.rs src/header.rs src/archive.rs src/extract.rs
-
-/home/cata-884/rust_tar/target/debug/deps/libtar_project-cdd244b994f2a8e0.rmeta: src/main.rs src/header.rs src/archive.rs src/extract.rs
-
-src/main.rs:
-src/header.rs:
-src/archive.rs:
-src/extract.rs:
